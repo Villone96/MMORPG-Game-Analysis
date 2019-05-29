6c614d49463cf9570bf11c97c19013e78f89237a
Fabrizio <fabry.olivadese@gmail.com>
2019-05-29 22:48:41 +0200

    Create e function for create a csv with the trend of users, messages, attack and trades

--- Dash/progetto_data.py
+++ Dash/progetto_data.py
@@ -4,6 +4,8 @@ import dash_html_components as html
 import pandas as pd
 import dash_table
 import os
+import plotly.graph_objs as go
+
 
 
 df = pd.read_csv('users_trend_30days.csv')
@@ -33,6 +35,35 @@ app.layout = html.Div(children=[
     html.H4(children='User trend in 30 days'),
     html.P('IL trend degli utenti nei 30 giorni è tendenzialmende descrescente'),
     html.P('La media degli utenti nei 30 giorni è di 2901'),
+
+    dcc.Markdown(),
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df['day'],
+                    y=df['users'],
+                    #text=df[df['continent'] == i]['country'],
+                    #text=df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    #name=i
+                ) 
+            ],
+            'layout': go.Layout(
+                xaxis={'title': 'Giorni'},
+                yaxis={'title': 'Numero di utenti'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    ),
 
     dcc.Graph(
     id='example-graph',
