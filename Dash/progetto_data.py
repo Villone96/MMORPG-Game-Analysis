9a858ed7fb469ecaacfb23bffc13812cfebafd4a
Fabrizio <fabry.olivadese@gmail.com>
2019-06-05 15:19:59 +0200

    cake bug fix

--- Dash/progetto_data.py
+++ Dash/progetto_data.py
@@ -100,7 +100,7 @@ app.layout = html.Div([
 
     dcc.Slider(
         id='day-selected',
-        min=0,
+        min=1,
         max=30,
         step=1,
         value=15,
@@ -155,7 +155,7 @@ app.layout = html.Div([
     [dash.dependencies.Input("day-selected", "value")])
 def update_output(value):
     return {
-        "data": [go.Pie(labels=edgesTrend["type"].unique().tolist(), values=edgesTrend[df["day"] == value]["quantity"].tolist(),
+        "data": [go.Pie(labels=edgesTrend["type"].unique().tolist(), values=edgesTrend[edgesTrend["day"] == value]["quantity"].tolist(),
                         marker={'colors': ['#EF963B', '#C93277', '#349600', '#EF533B']}, textinfo='label')],
         "layout": go.Layout(title=f"Trend report daily", margin={"l": 200, "r": 200, },
                             legend={"x": 1, "y": 0.7})}
